0628dfd2d30416b56efa55883f062851ec73fee1
sha1good <bankoleyakub11@yahoo.com>
2022-12-07 08:29:33 +0100

    Adding the backend service

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const passport = require("passport");
 
-const CLIENT_URL = "http://localhost:3000/";
+const CLIENT_URL = "https://social-login-frontend.netlify.app/";
 
 router.get("/login/success", (request, response) => {
   if (request.user) {
